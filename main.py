710c1b4a61ef249c89e9b91e858ed512788c194d
conrad mugabe <conradmugabe@gmail.com>
2025-01-25 06:11:29 +0300

    Add main entry point for FastAPI application

---
+++ main.py
@@ -0,0 +1,20 @@
+"""entry point for the application"""
+
+from fastapi import FastAPI
+
+app: FastAPI
+
+
+def main() -> None:
+    """main"""
+    global app
+
+    app = FastAPI()
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    main()
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
