4d371c8f35c1553726eff6bf19155e1d34a755d1
Ssejjemba Dan <ssejjembadan@gmail.com>
2025-02-06 02:27:35 +0300

    docs: Update documentation for models and schemas.

--- lib/config.py
+++ lib/config.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class AuthSettings(BaseSettings):
+    """Settings for authentication module"""
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
